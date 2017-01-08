31677cb040f89cee838d73f27e717077b1359e6a
tkekflaosldk <tkekflaosldk@gmail.com>
2017-01-08 23:25:05 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.port || 8080;
+var port = process.env.PORT;
 //var port = process.env.port || 80;
 //file
 var fs = require("fs");
